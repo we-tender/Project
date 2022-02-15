ef652eefa34e1850f5611b71cbcf2be334eedece
pcody <gioan92@naver.com>
2022-02-15 16:37:28 +0900

    네비게이션 스타일 변경

--- wetender/src/main/java/zemat/wetender/WetenderApplication.java
+++ wetender/src/main/java/zemat/wetender/WetenderApplication.java
@@ -16,15 +16,4 @@ public class WetenderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WetenderApplication.class, args);
 	}
-
-	// 작성자 설정란.. uuid를 추후에 멤버 아이디 또는 닉네임으로 변경하면 됨.
-//	@Bean
-	public AuditorAware<String> auditorProvider(){
-		return new AuditorAware<String>() {
-			@Override
-			public Optional<String> getCurrentAuditor() {
-				return Optional.of(UUID.randomUUID().toString());
-			}
-		};
-	}
 }
